89d3486f52d9b33e429de1ae56e3e66f2c2ffe27
Jeremy Kelleher <jkjak6@gmail.com>
2023-01-18 10:55:07 -0700

    add versionable modelling and tests from foundation

--- Sources/Versionable/Versionable.swift
+++ Sources/Versionable/Versionable.swift
@@ -1,6 +1,97 @@
-public struct Versionable {
-    public private(set) var text = "Hello, World!"
+//
+//  Versionable.swift
+//  
+//
+//  Created by Jeremy Kelleher on 1/13/23.
+//
 
-    public init() {
+import Foundation
+
+/// A protocol used for versioning model types that are `Codable`. This is done using
+/// a `version` property, typically implemented with an `enum` that contains
+/// an `extract` function that extracts the current model from the JSON and migrates it
+/// to the latest version. 
+public protocol Versionable: Codable, Equatable {
+    
+    /// The `CodingKey` implementation used to get the properties for this model out of the JSON
+    associatedtype CodingKeys: CodingKey
+    
+    /// A type used to represent the different versions that have been offered by this type.
+    associatedtype Version: VersionType<Self>
+    
+    /// The version of this instance.
+    ///
+    /// - Note: In code, we should always have the latest version of model schemas.
+    /// This property is only used to read the version from JSON in `Version.extract`
+    /// and decode using the proper decoders for that version to migrate the instance to the latest version.
+    /// - Note: This property can't be computed (say to always be the `Version.latest`
+    /// since computed properties can't be encoded using the automatic `Encodable` conformance.
+    var version: Version { get }
+    
+}
+
+extension Versionable {
+    
+    public init(from decoder: Decoder) throws {
+        
+        // get the version from the JSON
+        let versionContainer = try decoder.container(keyedBy: VersionableCodingKeys.self)
+        let serializedVersion = try versionContainer.decode(Self.Version.self, forKey: .version)
+        
+        // create a decoding container that can be used to extract the actual properties for this model
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        // extract the model using the version found in the JSON and migrate it to the latest version
+        self = try serializedVersion.extract(from: container)
+        
     }
+    
+    // Note: No encoding implementation is necessary as we always encode the latest version using the automatic encoder.
+    
+}
+
+/// A protocol that `Versionable` types use to represent versions.
+/// Typically, an `enum` is used to implement this protocol.
+/// The `extract(from:)` is used to extract and migrate JSON data to the latest version.
+public protocol VersionType<Model>: Codable, RawRepresentable<Int>, CaseIterable, Comparable, Hashable {
+    
+    /// The `Model` type that this version is referring to.
+    associatedtype Model: Versionable
+    
+    /// Extracts the model from the `container`.
+    ///
+    ///
+    /// This function migrates the model stored in the JSON to the latest version.
+    /// If the JSON is already using the latest version, that model is simply returned.
+    /// If not, the JSON is modifed to jump from the JSON's version to the latest version.
+    /// - Parameter container: The `Decoder` container given to use by `Codable.init(from: Decoder)`.
+    /// Conveniently, this function gets the already keyd container using the`Model.CodingKeys`
+    /// so the implementer doesn't need to parse this out.
+    /// - Returns: This function extracts the `Model` from the `container` and returns it.
+    func extract(from container: KeyedDecodingContainer<Model.CodingKeys>) throws -> Model
+    
+    /// An explanation of what this version changed.
+    var explanation: String { get }
+    
+}
+
+public extension VersionType {
+    
+    /// Conformance with `Comparable` to allow for finding the latest version.
+    static func < (a: Self, b: Self) -> Bool {
+        return a.rawValue < b.rawValue
+    }
+    
+    /// The latest (highest) version of this type.
+    static var latest: Self {
+        let allCases = Self.allCases.sorted()
+        return allCases[allCases.index(allCases.endIndex, offsetBy: -1)]
+    }
+    
+}
+
+
+/// A helper `CodingKey` used to extract just the `version` from `Versionable` JSON.
+enum VersionableCodingKeys: CodingKey {
+    case version
 }
---
+++ Sources/Versionable/VersionableTests.swift
@@ -0,0 +1,118 @@
+//
+//  VersionableTests.swift
+//  
+//
+//  Created by Jeremy Kelleher on 1/17/23.
+//
+
+import XCTest
+
+open class VersionableTests<Model>: XCTestCase where Model: Versionable, Model: Decodable {
+    
+    struct DecodingExpectation {
+        let model: Model
+        let json: String
+    }
+    
+    /// A dictionary with `DecodingExpectation` for each `Version`.
+    ///
+    /// This allows the tester to specify, for each version, what the JSON
+    /// should look like for a model and the output of decoding of that JSON.
+    /// Allows us to test all of the versions of this model and how decoding of files
+    /// containing any version of this model would be migrated forward to the latest version.
+    var expectations: [Model.Version: DecodingExpectation] {
+        [:] // empty since this would be implemented in subclasses of `VersionableTests`
+    }
+    
+    /// Tests that the versions are in order in the actual enum's implementation.
+    func test_Ordered_Versions() {
+
+        let allCases = Model.Version.allCases
+        
+        let sortedCases = allCases.sorted()
+        let arrayAllCases = allCases.map { $0 } // necessary since `allCases` is a special type: `Model.Version.AllCases`
+        
+        XCTAssertEqual(arrayAllCases, sortedCases, "The cases in \(Model.Version.self) should be in order. Update the ordering to be: \(sortedCases)")
+
+    }
+    
+    /// Tests that `Model.Version.latest` is using the last version in enum.
+    func test_Last_Version_Is_Latest_Version() throws {
+        
+        let lastCase = try XCTUnwrap(Model.Version.allCases.reversed().first)
+        XCTAssertEqual(lastCase, Model.Version.latest, "The last case (\(lastCase)) in \(Model.Version.self) should be the latest version.")
+        
+    }
+    
+    func test_Current_Model_Uses_Latest_Version() throws {
+        
+        let decodingExpectation = try XCTUnwrap(expectations[Model.Version.latest], "The latest version (\(Model.Version.latest)) of \(Model.self) did not have a decoding expectation.")
+        
+        let latestModel = decodingExpectation.model
+        
+        XCTAssertEqual(latestModel.version, Model.Version.latest, "Models created in code should always have the latest version. The \(Model.self) created in code for version \(Model.Version.latest) didn't have the latest version for it's `.version` property.")
+        
+    }
+    
+    func test_Encoding_Uses_Latest_Version() throws {
+        
+        let decodingExpectation = try XCTUnwrap(expectations[Model.Version.latest], "The latest version (\(Model.Version.latest)) of \(Model.self) did not have a decoding expectation.")
+        
+        let latestModel = decodingExpectation.model
+        
+        let encodedData = try JSONEncoder().encode(latestModel)
+        let decodedModel = try JSONDecoder().decode(Model.self, from: encodedData)
+        
+        XCTAssertEqual(decodedModel.version, Model.Version.latest, "The latest version (\(Model.Version.latest)) wasn't use in encoding \(Model.self)")
+        XCTAssertEqual(decodedModel, latestModel)
+        
+    }
+    
+    /// Tests that all versions have a sample JSON and the expected model
+    /// from decoding that JSON. Also ensures that all properties from
+    /// the expected model and the output of decoding the JSON match.
+    func test_Decoding_All_Versions() throws {
+
+        for version in Model.Version.allCases {
+
+            // ensures that all versions have test data
+            let decodingExpectation = try XCTUnwrap(expectations[version], "No input JSON and expected output were found for \(Model.self) version \(version)")
+
+            let output = try decode(json: decodingExpectation.json)
+            
+            // use `Mirror` to dynamically extract all of the properties of this object
+            let outputMirror = Mirror(reflecting: output)
+            let expectedOutputMirror = Mirror(reflecting: decodingExpectation.model)
+
+            // compare all of the properties in the decoded object to what was expected
+            for (outputProperty, expectedProperty) in zip(outputMirror.children, expectedOutputMirror.children) {
+                
+                guard outputProperty.label == expectedProperty.label else {
+                    XCTFail("The labels of properties in the output and expected models should match: \(String(describing: outputProperty.label)) != \(String(describing: expectedProperty.label))")
+                    continue // moves to the next property
+                }
+                
+                // need to use strings here since `.value` is `Any`.
+                let outputValue = String(describing: outputProperty.value)
+                let expectedValue = String(describing: expectedProperty.value)
+                
+                XCTAssertEqual(outputValue, expectedValue, "The output of decoding \(String(describing: outputProperty.label)) in version \(version) of \(Model.self) was \(outputValue) and it was expected to be \(expectedValue).")
+            }
+
+        }
+
+    }
+    
+    /// Decodes JSON to the `Model` type.
+    func decode(json: String) throws -> Model {
+        
+        let data = try XCTUnwrap(json.data(using: .utf8))
+        let output = try JSONDecoder().decode(Model.self, from: data)
+        
+        XCTAssertEqual(output.version, Model.Version.latest, "No matter, the input's version, the output decoding of a `Versionable` should have the `Version.latest` as it's `version`. Make sure to update \(Model.self).version to be \(Model.Version.latest).")
+                
+        return output
+        
+    }
+    
+}
